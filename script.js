1551eb4e2b772428aac61cbb8825311189f822ca
Frederic Pun <frederic.pun@gmail.com>
2018-03-05 15:01:05 -0500

    Finished Mobile (TODO: Testing and mobile nav)

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ let text = document.getElementById("nav-text");
 
 window.addEventListener("scroll", () => {
   if (document.body.scrollTop === 0) {
-    nav.style.height = "90px";
-    nav.style.backgroundColor = "transparent";
-    nav.style.boxShadow = "";
-
+    nav.classList.remove("nav-dropped");
     text.style.color = "white";
+    nav.style.color = "white";
   } else {
-    nav.style.height = "60px";
-    nav.style.backgroundColor = "white";
-    nav.style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
-
+    nav.classList.add("nav-dropped");
     text.style.color = "#3b3b3b";
+    nav.style.color = "#3b3b3b";
   }
 });
