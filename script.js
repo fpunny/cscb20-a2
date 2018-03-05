bf0d3b0af54b6e9b65b98990069d994b2189b9ed
Frederic Pun <frederic.pun@gmail.com>
2018-03-05 01:16:37 -0500

    for now

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let nav = document.getElementById("nav");
+
 window.addEventListener("scroll", () => {
-  let nav = document.getElementById("nav");
   if (document.body.scrollTop === 0) {
     nav.style.height = "90px";
+    nav.style.backgroundColor = "transparent";
+    nav.style.boxShadow = "";
   } else {
     nav.style.height = "60px";
+    nav.style.backgroundColor = "#108ce4";
+    nav.style.boxShadow = "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)";
   }
 });
